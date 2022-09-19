7d1aaa4a3d1ff85b5eb009f5bd3e48f83e7e1ffb
yeopto <gunyeop530@gmail.com>
2022-09-19 20:44:35 +0900

    [Chore] 환경 세팅 완료

---
+++ gatsby-node.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+// Setup Import Alias
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {};
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, 'src/components'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
+      },
+    },
+  });
+};
