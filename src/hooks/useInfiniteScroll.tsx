149c797c138527b0de99ca317b736412696753de
yeopto <gunyeop530@gmail.com>
2022-10-13 17:51:22 +0900

    [Fix] IntersectionObserver로 인한 build 실패 오류 해결

--- src/hooks/useInfiniteScroll.tsx
+++ src/hooks/useInfiniteScroll.tsx
@@ -31,29 +31,31 @@ const useInfiniteScroll = function (
     [selectedCategory],
   );
 
-  const observer: IntersectionObserver = new IntersectionObserver(
-    (entries, observer) => {
-      if (!entries[0].isIntersecting) return;
+  if (typeof window !== 'undefined') {
+    const observer: IntersectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        if (!entries[0].isIntersecting) return;
 
-      setCount(value => value + 1);
-      observer.disconnect();
-    },
-  );
-
-  useEffect(() => setCount(1), [selectedCategory])
-
-  useEffect(() => {
-    if (
-      NUMBER_OF_ITEMS_PER_PAGE * count >= postListByCategory.length ||
-      containerRef.current === null ||
-      containerRef.current.children.length === 0
-    )
-      return;
-
-    observer.observe(
-      containerRef.current.children[containerRef.current.children.length - 1],
+        setCount(value => value + 1);
+        observer.disconnect();
+      },
     );
-  }, [count, selectedCategory]);
+
+    useEffect(() => {
+      if (
+        NUMBER_OF_ITEMS_PER_PAGE * count >= postListByCategory.length ||
+        containerRef.current === null ||
+        containerRef.current.children.length === 0
+      )
+        return;
+
+      observer.observe(
+        containerRef.current.children[containerRef.current.children.length - 1],
+      );
+    }, [count, selectedCategory]);
+  }
+
+  useEffect(() => setCount(1), [selectedCategory]);
 
   return {
     containerRef,
