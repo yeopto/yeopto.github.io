a120b9a2cc326ec36a8dba152cdcf33c232144da
yeopto <gunyeop530@gmail.com>
2022-10-18 19:17:44 +0900

    [Feat] SEO 설정 완료

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -2,20 +2,48 @@ import React, { FunctionComponent, ReactNode } from 'react';
 import styled from '@emotion/styled';
 import GlobalStyle from 'components/Common/GlobalStyle';
 import Footer from 'components/Common/Footer';
+import { Helmet } from 'react-helmet';
 
 type TemplateProps = {
+  title: string;
+  description: string;
+  url: string;
+  image: string;
   children: ReactNode;
 };
 
-const Container = styled.div`
+const Container = styled.main`
   display: flex;
   flex-direction: column;
   height: 100%;
 `;
 
-const Template: FunctionComponent<TemplateProps> = function ({ children }) {
+const Template: FunctionComponent<TemplateProps> = function ({
+  children,
+  title,
+  description,
+  url,
+  image,
+}) {
   return (
     <Container>
+      <Helmet>
+        <title>{title}</title>
+
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta property="og:url" content={url} />
+        <meta property="og:site_name" content={title} />
+        <meta name="google-site-verification" content="ql22z_2Y_hxY79KKMHs_pLQVmom92VRl7YZ-86hVhZo" />
+
+        <html lang="ko" />
+      </Helmet>
       <GlobalStyle />
       {children}
       <Footer />
